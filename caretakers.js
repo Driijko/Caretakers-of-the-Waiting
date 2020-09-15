6658843b1eb611e2f67854b0eb9cb62e2ca4bf71
Driijko <driijko@gmail.com>
2020-09-15 11:40:09 -0400

    Setup initial view

---
+++ caretakers.js
@@ -0,0 +1,19 @@
+let mode;
+
+function preload() {
+
+}
+
+function setup() {
+
+  // Initial view
+  document.documentElement.style.overflow = "hidden";
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  mode = "openingPrompt";
+}
+
+function draw() {
+
+}
