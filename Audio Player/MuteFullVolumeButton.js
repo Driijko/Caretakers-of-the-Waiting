2a6ff8881d1f6aa566170909cfe98ba6e041472d
Driijko <driijko@gmail.com>
2020-09-22 13:22:29 -0400

    Make play-pause button change image when clicked in either layout 0 or layout 1

--- Audio Player/MuteFullVolumeButton.js
+++ Audio Player/MuteFullVolumeButton.js
@@ -1,13 +1,18 @@
 function MuteFullVolumeButton(layout) {
 
     this.layout = layout;
+    this.image = imFullVolumeButton;
 
     this.display = function() {
         if (this.layout === 0) {
-            image(imMuteButton, (width/2) - 140, height - 105, 40, 40)
+            image(this.image, (width/2) - 140, height - 105, 40, 40)
         }
         else if (this.layout === 1) {
-            image(imMuteButton, (width/2) + 80, height - 100, 40, 40)
+            image(this.image, (width/2) + 80, height - 100, 40, 40)
         }
+    }
+
+    this.input = function() {
+        // if (this.)
     }
 }
--- Audio Player/PlayPauseButton.js
+++ Audio Player/PlayPauseButton.js
@@ -10,13 +10,23 @@ function PlayPauseButton(layout) {
         }
         else if (this.layout === 1) {
             image(this.image, (width/2) - 350, height - 100, 40, 40);
+            noStroke();
+            fill(255, 50);
+            ellipse((width/2) - 330, height - 80, 40);
         }
     }
 
     this.input = function() {
         if (
-            this.layout === 0
-            && collidePointCircle(mouseX, mouseY, (width/2) - 120, height - 155, 40)
+            ((
+                this.layout === 0
+                && collidePointCircle(mouseX, mouseY, (width/2) - 120, height - 155, 40)
+            )
+            ||
+            (
+                this.layout === 1
+                && collidePointCircle(mouseX, mouseY, (width/2) - 330, height - 80, 40)
+            ))
             && mouse.clicked
         ) {
             if (this.image === imPlayButton) {
